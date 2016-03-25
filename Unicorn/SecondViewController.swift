005330abf1c64457506244ab5c00bc7d8ce53514
HerbaN <mna271@nyu.edu>
2016-03-24 21:22:59 -0400

    Initial Commit

---
+++ Unicorn/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Unicorn
+//
+//  Created by Mir Ali on 3/24/16.
+//  Copyright © 2016 Herban. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
